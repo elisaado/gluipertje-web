e354047a31c79780f564e272ef5b6913e7ce2f65
Eli Saado <eli5saado@gmail.com>
2018-03-16 20:36:27 +0100

    Make it check if the user is already at bottom of page and format the js

--- js/app.js
+++ js/app.js
@@ -17,15 +17,22 @@ function refreshMessages() {
       for (let rawMessage of rawMessages) {
         messages.push(`<div class="card mx-auto"><div class="card-body text-left"><h5 class="card-title">${rawMessage.from.nickname}</h5><h6 class="card-subtitle mb-2 text-muted">(@${rawMessage.from.username})</h6><br><p class="card-text">${rawMessage.body}</p></div></div><br>`);
       }
+
+      // Check if there are new messages
       if (messages.length != app.messages.length || app.messages.length == 0) {
         messages.reverse();
         app.messages = messages;
-        console.log($(document).height());
-        $("html").animate({scrollTop: $(document).height()}, "slow");
+
+        // Check if the user is already at the bottom of the page so that if they are reading older messages it doesn't scroll down when a new message appears
+        if ($(document).height() - $(window).scrollTop() < 800) { // This feels like a hack
+          $("html").animate({
+            scrollTop: $(document).height()
+          }, "slow");
+        }
       }
     });
 }
 
-$(document).ready(function () {
+$(document).ready(function() {
   setInterval(refreshMessages, 1000);
 });
