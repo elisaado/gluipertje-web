60f86fd854822032ec9cf9ffd611b7de4c665bbe
Eli Saado <elimalkosaado@gmail.com>
2018-09-19 13:57:33 +0200

    yay

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-!function(){var e={de_DE:{days:["Sonntag","Montag","Dienstag","Mittwoch","Donnerstag","Freitag","Samstag"],shortDays:["So","Mo","Di","Mi","Do","Fr","Sa"],months:["Januar","Februar","März","April","Mai","Juni","Juli","August","September","Oktober","November","Dezember"],shortMonths:["Jan","Feb","Mär","Apr","Mai","Jun","Jul","Aug","Sep","Okt","Nov","Dez"],AM:"AM",PM:"PM",am:"am",pm:"pm",formats:{c:"%a %d %b %Y %X %Z",D:"%d.%m.%Y",F:"%Y-%m-%d",R:"%H:%M",r:"%I:%M:%S %p",T:"%H:%M:%S",v:"%e-%b-%Y",X:"%T",x:"%D"}},en_CA:{days:["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"],shortDays:["Sun","Mon","Tue","Wed","Thu","Fri","Sat"],months:["January","February","March","April","May","June","July","August","September","October","November","December"],shortMonths:["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],ordinalSuffixes:["st","nd","rd","th","th","th","th","th","th","th","th","th","th","th","th","th","th","th","th","th","st","nd","rd","th","th","th","th","th","th","th","st"],AM:"AM",PM:"PM",am:"am",pm:"pm",formats:{c:"%a %d %b %Y %X %Z",D:"%d/%m/%y",F:"%Y-%m-%d",R:"%H:%M",r:"%I:%M:%S %p",T:"%H:%M:%S",v:"%e-%b-%Y",X:"%r",x:"%D"}},en_US:{days:["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"],shortDays:["Sun","Mon","Tue","Wed","Thu","Fri","Sat"],months:["January","February","March","April","May","June","July","August","September","October","November","December"],shortMonths:["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],ordinalSuffixes:["st","nd","rd","th","th","th","th","th","th","th","th","th","th","th","th","th","th","th","th","th","st","nd","rd","th","th","th","th","th","th","th","st"],AM:"AM",PM:"PM",am:"am",pm:"pm",formats:{c:"%a %d %b %Y %X %Z",D:"%m/%d/%y",F:"%Y-%m-%d",R:"%H:%M",r:"%I:%M:%S %p",T:"%H:%M:%S",v:"%e-%b-%Y",X:"%r",x:"%D"}},es_MX:{days:["domingo","lunes","martes","miércoles","jueves","viernes","sábado"],shortDays:["dom","lun","mar","mié","jue","vie","sáb"],months:["enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre"," diciembre"],shortMonths:["ene","feb","mar","abr","may","jun","jul","ago","sep","oct","nov","dic"],AM:"AM",PM:"PM",am:"am",pm:"pm",formats:{c:"%a %d %b %Y %X %Z",D:"%d/%m/%Y",F:"%Y-%m-%d",R:"%H:%M",r:"%I:%M:%S %p",T:"%H:%M:%S",v:"%e-%b-%Y",X:"%T",x:"%D"}},fr_FR:{days:["dimanche","lundi","mardi","mercredi","jeudi","vendredi","samedi"],shortDays:["dim.","lun.","mar.","mer.","jeu.","ven.","sam."],months:["janvier","février","mars","avril","mai","juin","juillet","août","septembre","octobre","novembre","décembre"],shortMonths:["janv.","févr.","mars","avril","mai","juin","juil.","août","sept.","oct.","nov.","déc."],AM:"AM",PM:"PM",am:"am",pm:"pm",formats:{c:"%a %d %b %Y %X %Z",D:"%d/%m/%Y",F:"%Y-%m-%d",R:"%H:%M",r:"%I:%M:%S %p",T:"%H:%M:%S",v:"%e-%b-%Y",X:"%T",x:"%D"}},it_IT:{days:["domenica","lunedì","martedì","mercoledì","giovedì","venerdì","sabato"],shortDays:["dom","lun","mar","mer","gio","ven","sab"],months:["gennaio","febbraio","marzo","aprile","maggio","giugno","luglio","agosto","settembre","ottobre","novembre","dicembre"],shortMonths:["pr","mag","giu","lug","ago","set","ott","nov","dic"],AM:"AM",PM:"PM",am:"am",pm:"pm",formats:{c:"%a %d %b %Y %X %Z",D:"%d/%m/%Y",F:"%Y-%m-%d",R:"%H:%M",r:"%I:%M:%S %p",T:"%H:%M:%S",v:"%e-%b-%Y",X:"%T",x:"%D"}},nl_NL:{days:["zondag","maandag","dinsdag","woensdag","donderdag","vrijdag","zaterdag"],shortDays:["zo","ma","di","wo","do","vr","za"],months:["januari","februari","maart","april","mei","juni","juli","augustus","september","oktober","november","december"],shortMonths:["jan","feb","mrt","apr","mei","jun","jul","aug","sep","okt","nov","dec"],AM:"AM",PM:"PM",am:"am",pm:"pm",formats:{c:"%a %d %b %Y %X %Z",D:"%d-%m-%y",F:"%Y-%m-%d",R:"%H:%M",r:"%I:%M:%S %p",T:"%H:%M:%S",v:"%e-%b-%Y",X:"%T",x:"%D"}},pt_BR:{days:["domingo","segunda","terça","quarta","quinta","sexta","sábado"],shortDays:["Dom","Seg","Ter","Qua","Qui","Sex","Sáb"],months:["janeiro","fevereiro","março","abril","maio","junho","julho","agosto","setembro","outubro","novembro","dezembro"],shortMonths:["Jan","Fev","Mar","Abr","Mai","Jun","Jul","Ago","Set","Out","Nov","Dez"],AM:"AM",PM:"PM",am:"am",pm:"pm",formats:{c:"%a %d %b %Y %X %Z",D:"%d-%m-%Y",F:"%Y-%m-%d",R:"%H:%M",r:"%I:%M:%S %p",T:"%H:%M:%S",v:"%e-%b-%Y",X:"%T",x:"%D"}},ru_RU:{days:["Воскресенье","Понедельник","Вторник","Среда","Четверг","Пятница","Суббота"],shortDays:["Вс","Пн","Вт","Ср","Чт","Пт","Сб"],months:["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"],shortMonths:["янв","фев","мар","апр","май","июн","июл","авг","сен","окт","ноя","дек"],AM:"AM",PM:"PM",am:"am",pm:"pm",formats:{c:"%a %d %b %Y %X",D:"%d.%m.%y",F:"%Y-%m-%d",R:"%H:%M",r:"%I:%M:%S %p",T:"%H:%M:%S",v:"%e-%b-%Y",X:"%T",x:"%D"}},tr_TR:{days:["Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi"],shortDays:["Paz","Pzt","Sal","Çrş","Prş","Cum","Cts"],months:["Ocak","Şubat","Mart","Nisan","Mayıs","Haziran","Temmuz","Ağustos","Eylül","Ekim","Kasım","Aralık"],shortMonths:["Oca","Şub","Mar","Nis","May","Haz","Tem","Ağu","Eyl","Eki","Kas","Ara"],AM:"ÖÖ",PM:"ÖS",am:"ÖÖ",pm:"ÖS",formats:{c:"%a %d %b %Y %X %Z",D:"%d-%m-%Y",F:"%Y-%m-%d",R:"%H:%M",r:"%I:%M:%S %p",T:"%H:%M:%S",v:"%e-%b-%Y",X:"%T",x:"%D"}},zh_CN:{days:["星期日","星期一","星期二","星期三","星期四","星期五","星期六"],shortDays:["日","一","二","三","四","五","六"],months:["一月份","二月份","三月份","四月份","五月份","六月份","七月份","八月份","九月份","十月份","十一月份","十二月份"],shortMonths:["一月","二月","三月","四月","五月","六月","七月","八月","九月","十月","十一月","十二月"],AM:"上午",PM:"下午",am:"上午",pm:"下午",formats:{c:"%a %d %b %Y %X %Z",D:"%d/%m/%y",F:"%Y-%m-%d",R:"%H:%M",r:"%I:%M:%S %p",T:"%H:%M:%S",v:"%e-%b-%Y",X:"%r",x:"%D"}}},a=e.en_US,r=new function r(d,h,M){var c,b=d||a,l=h||0,f=M||!1,g=0;var y=function(e,a){var r;if(a){if(r=a.getTime(),f){var d=u(a);if(u(a=new Date(r+d+l))!==d){var h=u(a);a=new Date(r+h+l)}}}else{var M=Date.now();M>g?(g=M,c=new Date(g),r=g,f&&(c=new Date(g+u(c)+l))):r=g,a=c}return function e(a,r,u,d){var h="",M=null,c=!1,b=a.length,g=!1;for(var y=0;y<b;y++){var v=a.charCodeAt(y);if(!0!==c)37!==v?h+=a[y]:c=!0;else{if(45===v){M="";continue}if(95===v){M=" ";continue}if(48===v){M="0";continue}if(58===v){g&&i("[WARNING] detected use of unsupported %:: or %::: modifiers to strftime"),g=!0;continue}switch(v){case 37:h+="%";break;case 65:h+=u.days[r.getDay()];break;case 66:h+=u.months[r.getMonth()];break;case 67:h+=t(Math.floor(r.getFullYear()/100),M);break;case 68:h+=e(u.formats.D,r,u,d);break;case 70:h+=e(u.formats.F,r,u,d);break;case 72:h+=t(r.getHours(),M);break;case 73:h+=t(s(r.getHours()),M);break;case 76:h+=o(Math.floor(d%1e3));break;case 77:h+=t(r.getMinutes(),M);break;case 80:h+=r.getHours()<12?u.am:u.pm;break;case 82:h+=e(u.formats.R,r,u,d);break;case 83:h+=t(r.getSeconds(),M);break;case 84:h+=e(u.formats.T,r,u,d);break;case 85:h+=t(n(r,"sunday"),M);break;case 87:h+=t(n(r,"monday"),M);break;case 88:h+=e(u.formats.X,r,u,d);break;case 89:h+=r.getFullYear();break;case 90:if(f&&0===l)h+="GMT";else{var D=r.toString().match(/\(([\w\s]+)\)/);h+=D&&D[1]||""}break;case 97:h+=u.shortDays[r.getDay()];break;case 98:h+=u.shortMonths[r.getMonth()];break;case 99:h+=e(u.formats.c,r,u,d);break;case 100:h+=t(r.getDate(),M);break;case 101:h+=t(r.getDate(),null==M?" ":M);break;case 104:h+=u.shortMonths[r.getMonth()];break;case 106:var p=new Date(r.getFullYear(),0,1),S=Math.ceil((r.getTime()-p.getTime())/864e5);h+=o(S);break;case 107:h+=t(r.getHours(),null==M?" ":M);break;case 108:h+=t(s(r.getHours()),null==M?" ":M);break;case 109:h+=t(r.getMonth()+1,M);break;case 110:h+="\n";break;case 111:var S=r.getDate();u.ordinalSuffixes?h+=String(S)+(u.ordinalSuffixes[S-1]||m(S)):h+=String(S)+m(S);break;case 112:h+=r.getHours()<12?u.AM:u.PM;break;case 114:h+=e(u.formats.r,r,u,d);break;case 115:h+=Math.floor(d/1e3);break;case 116:h+="\t";break;case 117:var S=r.getDay();h+=0===S?7:S;break;case 118:h+=e(u.formats.v,r,u,d);break;case 119:h+=r.getDay();break;case 120:h+=e(u.formats.x,r,u,d);break;case 121:h+=(""+r.getFullYear()).slice(2);break;case 122:if(f&&0===l)h+=g?"+00:00":"+0000";else{var k,Y=(k=0!==l?l/6e4:-r.getTimezoneOffset())<0?"-":"+",A=g?":":"",T=Math.floor(Math.abs(k/60)),F=Math.abs(k%60);h+=Y+t(T)+A+t(F)}break;default:c&&(h+="%"),h+=a[y]}M=null,c=!1}}return h}(e,a,b,r)};y.localize=function(e){return new r(e||b,l,f)};y.localizeByIdentifier=function(a){var r=e[a];return r?y.localize(r):(i('[WARNING] No locale found with identifier "'+a+'".'),y)};y.timezone=function(e){var a=l,t=f,o=typeof e;if("number"===o||"string"===o)if(t=!0,"string"===o){var s="-"===e[0]?-1:1,n=parseInt(e.slice(1,3),10),m=parseInt(e.slice(3,5),10);a=s*(60*n+m)*60*1e3}else"number"===o&&(a=60*e*1e3);return new r(b,a,t)};y.utc=function(){return new r(b,l,!0)};return y}(a,0,!1);function t(e,a){return""===a||e>9?e:(null==a&&(a="0"),a+e)}function o(e){return e>99?e:e>9?"0"+e:"00"+e}function s(e){return 0===e?12:e>12?e-12:e}function n(e,a){a=a||"sunday";var r=e.getDay();"monday"===a&&(0===r?r=6:r--);var t=Date.UTC(e.getFullYear(),0,1),o=Date.UTC(e.getFullYear(),e.getMonth(),e.getDate()),s=(Math.floor((o-t)/864e5)+7-r)/7;return Math.floor(s)}function m(e){var a=e%10,r=e%100;if(r>=11&&r<=13||0===a||a>=4)return"th";switch(a){case 1:return"st";case 2:return"nd";case 3:return"rd"}}function u(e){return 6e4*(e.getTimezoneOffset()||0)}function i(e){"undefined"!=typeof console&&"function"==typeof console.warn&&console.warn(e)}"undefined"!=typeof module?module.exports=r:(function(){return this||(0,eval)("this")}()).strftime=r,"function"!=typeof Date.now&&(Date.now=function(){return+new Date})}();
-
-let gluipertje = new Gluipertje("https://gluipertje.elisaado.com", 443);
+let gluipertje = new Gluipertje({host: "https://gluipertje.elisaado.com", port: 443});
 // let gluipertje = new Gluipertje("http://0.0.0.0", 8000); // Test server
+
+let rf = new IntlRelativeFormat('en-US');
 
 function checkVisible(elm) {
   var rect = elm.getBoundingClientRect();
@@ -86,8 +86,8 @@ function checkUsername() {
     return;
   }
 
-  gluipertje.user.all()
-    .then(function(users) {
+  gluipertje.getAllUsers()
+    .then((users) => {
       let a = true;
       for (let user of users) {
         if (user.username == $("#registerUsername").val()) {
@@ -111,9 +111,10 @@ function checkUsername() {
 function register() {
   checkUsername();
 
-  gluipertje.user.create($("#registerNickname").val(), $("#registerUsername").val(), $("#registerPassword").val()).then(function(user) {
-    gluipertje.user.byToken(user.token)
-      .then(function(safeUser) {
+  gluipertje.createUser({nickname: $("#registerNickname").val(), username: $("#registerUsername").val(), password: $("#registerPassword").val()})
+  .then((user) => {
+    gluipertje.getUserByToken(user.token)
+      .then((safeUser) => {
         if (!safeUser.id) {
           return false;
         }
@@ -131,29 +132,27 @@ function register() {
 
 // "Logs in"
 function login() {
-  gluipertje.user.revokeToken($("#loginUsername").val(), $("#loginPassword").val())
-    .then(function(token) {
-      gluipertje.user.byToken(token)
-        .then(function(user) {
-          if (!user.id) {
-            if (user == "Invalid token") {
-              $("#loginAlertText").html("The username and password do not match");
-              $("#loginAlert").addClass("alert-danger");
-            }
-            $("#loginAlert").show();
+  gluipertje.revokeToken({username: $("#loginUsername").val(), password: $("#loginPassword").val()})
+    .then((token) => gluipertje.getUserByToken(token))
+    .then((user) => {
+      if (!user.id) {
+        if (user == "Invalid token") {
+          $("#loginAlertText").html("The username and password do not match");
+          $("#loginAlert").addClass("alert-danger");
+        }
+        $("#loginAlert").show();
 
-            $("#loginAlert").alert();
-            return;
-          }
-          app.user = user;
-          localStorage.setItem("token", token);
-          $("#userDropdown").show();
-          $("#messageInput, #messageButton").prop("disabled", false);
-          clearInterval(refreshMessagesInterval);
-          refreshMessagesInterval = setInterval(refreshMessages, 1000);
-          $("#messageButton").click(sendMessage);
-          $("#loginModal").modal("hide");
-        });
+        $("#loginAlert").alert();
+        return;
+      }
+      app.user = user;
+      localStorage.setItem("token", token);
+      $("#userDropdown").show();
+      $("#messageInput, #messageButton").prop("disabled", false);
+      clearInterval(refreshMessagesInterval);
+      refreshMessagesInterval = setInterval(refreshMessages, 1000);
+      $("#messageButton").click(sendMessage);
+      $("#loginModal").modal("hide");
     });
   return false;
 }
@@ -169,7 +168,7 @@ function sendMessage() {
   if (app.user.id == 0 || !localStorage.getItem("token") || $("#messageInput").val().length == 0) {
     return false;
   }
-  gluipertje.message.send(localStorage.getItem("token"), $("#messageInput").val().replace(/\n/g, "\\n"));
+  gluipertje.sendMessage({token: localStorage.getItem("token"), text: $("#messageInput").val().replace(/\n/g, "\\n")});
 
   refreshMessages(); // :)
 
@@ -192,21 +191,31 @@ function escapeHtml(html) {
 function refreshMessages(callb) {
   let messages = [];
 
-  gluipertje.message.all()
-    .then(function(rawMessages) {
+  gluipertje.getAllMessages()
+    .then((rawMessages) => {
       for (let rawMessage of rawMessages) {
-        words = escapeHtml(rawMessage.body).split(/( |\\n)/)
+        words = escapeHtml(rawMessage.text).split(/( |\\n)/)
 
         for (i = 0; i < words.length; i++) {
-          match = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/.exec(words[i])
+          match = (/((?:(http|https|Http|Https|rtsp|Rtsp):\/\/(?:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,64}(?:\:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,25})?\@)?)?((?:(?:[a-zA-Z0-9][a-zA-Z0-9\-]{0,64}\.)+(?:(?:aero|arpa|asia|a[cdefgilmnoqrstuwxz])|(?:biz|b[abdefghijmnorstvwyz])|(?:cat|com|coop|c[acdfghiklmnoruvxyz])|d[ejkmoz]|(?:edu|e[cegrstu])|f[ijkmor]|(?:gov|g[abdefghilmnpqrstuwy])|h[kmnrtu]|(?:info|int|i[delmnoqrst])|(?:jobs|j[emop])|k[eghimnrwyz]|l[abcikrstuvy]|(?:mil|mobi|museum|m[acdghklmnopqrstuvwxyz])|(?:name|net|n[acefgilopruz])|(?:org|om)|(?:pro|p[aefghklmnrstwy])|qa|r[eouw]|s[abcdeghijklmnortuvyz]|(?:tel|travel|t[cdfghjklmnoprtvwz])|u[agkmsyz]|v[aceginu]|w[fs]|y[etu]|z[amw]))|(?:(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[0-9])))(?:\:\d{1,5})?)(\/(?:(?:[a-zA-Z0-9\;\/\?\:\@\&\=\#\~\-\.\+\!\*\'\(\)\,\_])|(?:\%[a-fA-F0-9]{2}))*)?(?:\b|$)/gi).exec(words[i])
           if (match) {
-            words[i] = words[i].replace(match[0], `<a href="${match[0]}">${match[0]}</a>`)
+            let link = "";
+            if (!match[0].match(/^https?/)) {
+              link = `http://${match[0]}`; // most websites redirect http to https automatically if they support it
+            }
+            words[i] = words[i].replace(match[0], `<a target="_blank" href="${link}">${match[0]}</a>`)
           }
         }
 
-        rawMessage.body = words.join(" ").replace(/\\n/g, "<br>")
+        rawMessage.text = words.join(" ").replace(/\\n/g, "<br>")
+        let messageDate = "";
+        if((new Date().getTime() / 1000) - rawMessage.created_at.getTime() / 1000 > 60*60*24*7) {
+          messageDate = rawMessage.created_at.toDateString();
+        } else {
+          messageDate = rf.format(rawMessage.created_at);
+        }
 
-        messages.push(`<div class="card mx-4"><div class="card-body text-left"><h5 class="card-title">${escapeHtml(rawMessage.from.nickname)}</h5><h6 class="card-subtitle mb-2 text-muted">(@${escapeHtml(rawMessage.from.username)})</h6><br><p class="card-text">${rawMessage.body}</p></div><div class="card-footer text-muted">${strftime("%A at %H:%M", rawMessage.created_at)}</div></div><br>`);
+        messages.push(`<div class="card mx-4"><div class="card-body text-left"><h5 class="card-title">${escapeHtml(rawMessage.from.nickname)}</h5><h6 class="card-subtitle mb-2 text-muted">(@${escapeHtml(rawMessage.from.username)})</h6><br><p class="card-text">${rawMessage.text}</p></div><div class="card-footer text-muted">${messageDate}</div></div><br>`);
       }
 
       // Check if there are new messages
@@ -230,8 +239,8 @@ function refreshMessages(callb) {
 
 $(document).ready(function() {
   let token = localStorage.getItem("token");
-  gluipertje.user.byToken(token)
-    .then(function(user) {
+  gluipertje.getUserByToken(token)
+    .then((user) => {
       if (user.id) {
         app.user = user;
         $("#messageButton").click(sendMessage);
@@ -245,7 +254,7 @@ $(document).ready(function() {
       }
 
       // Only fetch messages once in the begin
-      refreshMessages(function() {
+      refreshMessages(() => {
         setTimeout(refreshMessagesInterval = setInterval(refreshMessages, interval), interval);
       });
     });
